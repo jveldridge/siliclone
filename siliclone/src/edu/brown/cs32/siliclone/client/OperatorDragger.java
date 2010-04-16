9041d2770c8704960537e22c954196595857d9be
douglas_hall@brown.edu <douglas_hall@brown.edu@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-04-16 03:06:18 +0000

    d+d added to - compiles, doesn't work

--- siliclone/src/edu/brown/cs32/siliclone/client/OperatorDragger.java
+++ siliclone/src/edu/brown/cs32/siliclone/client/OperatorDragger.java
@@ -1,29 +1,42 @@
 package edu.brown.cs32.siliclone.client;
 
 
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.smartgwt.client.types.DragAppearance;
-import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.events.ShowContextMenuEvent;
 import com.smartgwt.client.widgets.events.ShowContextMenuHandler;
 
 import edu.brown.cs32.siliclone.interfaces.Operator;
 
+
+
 public class OperatorDragger extends Canvas {
 	private Operator op;
 	public OperatorDragger(Operator op){
 		this.op = op;
-		addChild(op.getWidget());
+		
+		Widget opWidget = op.getWidget();
+		addChild(opWidget);
 		setShowEdges(true);
 		setCanDragReposition(true);
 		setKeepInParentRect(true);
 		setDragAppearance(DragAppearance.TARGET);
+		setHeight(opWidget.getOffsetHeight());
+		setWidth(opWidget.getOffsetWidth());
+		
+		
+		addShowContextMenuHandler(new RightClickHandler());
 	}
 	private class RightClickHandler implements ShowContextMenuHandler {
 		@Override
 		public void onShowContextMenu(ShowContextMenuEvent event) {
 			// TODO Auto-generated method stub
-			
+			PopupPanel p = new PopupPanel();
+			p.add(op.getPropertiesSelector());
+			p.show();
+			p.hide();
 		}
 	}
 	
---
+++ siliclone/src/edu/brown/cs32/siliclone/implementations/TestOp.java
@@ -0,0 +1,54 @@
+package edu.brown.cs32.siliclone.implementations;
+
+import java.util.Collection;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.smartgwt.client.widgets.Label;
+
+import edu.brown.cs32.siliclone.interfaces.DNASequence;
+import edu.brown.cs32.siliclone.interfaces.Operator;
+
+public class TestOp implements Operator {
+	private String name;
+	
+	public TestOp(String name){
+		this.name = name;
+	}
+	
+	@Override
+	public void calculate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Collection<Operator> getAdjacentOperators() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Widget getPropertiesSelector() {
+		// TODO Auto-generated method stub
+		return new Label("Properties Selector");
+	}
+
+	@Override
+	public Widget getWidget() {
+		// TODO Auto-generated method stub
+		return new Label(name);
+	}
+
+	@Override
+	public void setInput(int slotNum, Collection<DNASequence> input) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
