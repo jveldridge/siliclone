4f63d2e66b5fb235155fed7a6485f7763f15bcf8
douglas_hall@brown.edu <douglas_hall@brown.edu@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-04-16 00:32:08 +0000

    Separated ui

--- trunk/siliclone/src/edu/brown/cs32/siliclone/client/Siliclone.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/client/Siliclone.java
@@ -1,16 +1,37 @@
 package edu.brown.cs32.siliclone.client;
 
+
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.VLayout;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Siliclone implements EntryPoint {
-
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
 
+        VLayout mainLayout = new VLayout();  
+        mainLayout.setWidth100();  
+        mainLayout.setHeight100();  
+  
+        //top menu
+        mainLayout.addMember(new TopMenu());  
+  
+        HLayout notMenuLayout = new HLayout();  
+        notMenuLayout.setHeight("*");  
+
+        notMenuLayout.addMember(new ListingLabel());  
+        notMenuLayout.addMember(new Workspace()); 
+  
+        mainLayout.addMember(notMenuLayout);  
+        RootPanel.get().add(mainLayout);
+  
+       
 	}
 }
---
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/client/Workspace.java
@@ -0,0 +1,66 @@
+package edu.brown.cs32.siliclone.client;
+
+import com.smartgwt.client.util.SC; //very handy for debug
+
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.events.*;
+public class Workspace extends Canvas {
+	public Workspace(){
+		setCanAcceptDrop(true);
+		
+		setHeight100();
+		setWidth100();
+		setShowEdges(true);
+		
+		addDropHandler(new WorkDropHandle());
+		addShowContextMenuHandler(new RightClickHandler());
+	}
+	
+	private class WorkDropHandle implements DropHandler {
+		@Override
+		public void onDrop(DropEvent event) {
+			// TODO Auto-generated method stub
+			SC.say("Something was added to the workspace");
+		}
+	}
+	private class RightClickHandler implements ShowContextMenuHandler {
+		@Override
+		public void onShowContextMenu(ShowContextMenuEvent event) {
+			// TODO Auto-generated method stub
+			event.cancel();
+			SC.say("No right clicking!"); 
+		}
+	}
+	
+}
+
+
+/* {
+public Canvas(){
+             setCanAcceptDrop(true);  
+  
+             addShowContextMenuHandler(new ShowContextMenuHandler() {  
+                 public void onShowContextMenu(ShowContextMenuEvent event) {  
+                     event.cancel();  
+                 }  
+             });  
+             addDropOverHandler(new DropOverHandler() {  
+                 public void onDropOver(DropOverEvent event) {  
+                     //if (willAcceptDrop())  
+                         setBackgroundColor("#ffff80");  
+                 }  
+             });  
+             addDropOutHandler(new DropOutHandler() {  
+                 public void onDropOut(DropOutEvent event) {  
+                     setBackgroundColor("#ffffff");  
+                 }  
+             });  
+             addDropHandler(new DropHandler() {  
+                 public void onDrop(DropEvent event) {  
+                     addChild(new Label("Test"));
+                       
+                 }  
+             });  
+}
+}; 
+*/   
