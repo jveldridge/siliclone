eb6c8cd95950c060ec7807ebed0a70e08788072f
douglas_hall@brown.edu <douglas_hall@brown.edu@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-04-16 15:38:04 +0000

    Fixed properties selector popup.

--- trunk/siliclone/src/edu/brown/cs32/siliclone/client/OperatorDragger.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/client/OperatorDragger.java
@@ -1,9 +1,14 @@
 package edu.brown.cs32.siliclone.client;
 
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.smartgwt.client.types.DragAppearance;
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.events.ShowContextMenuEvent;
 import com.smartgwt.client.widgets.events.ShowContextMenuHandler;
 import com.smartgwt.client.widgets.layout.Layout;
@@ -14,6 +19,7 @@ import edu.brown.cs32.siliclone.interfaces.Operator;
 
 public class OperatorDragger extends Layout {
 	private Operator op;
+	private Layout selector;
 	public OperatorDragger(Operator op){
 		this.op = op;
 		
@@ -25,16 +31,37 @@ public class OperatorDragger extends Layout {
 		setDragAppearance(DragAppearance.TARGET);
 		
 		
+		selector = new Layout();
+		selector.setTitle("Properties Selector:");
+		selector.addMember(op.getPropertiesSelector());
+		selector.addMember(new Button("close", new HideSelectorHandler()));
+		selector.setShowEdges(true);
+		selector.setCanDragReposition(true);
+		selector.setKeepInParentRect(true);
+		selector.setDragAppearance(DragAppearance.TARGET);
+		selector.hide();
+		
 		addShowContextMenuHandler(new RightClickHandler());
 	}
+	public Layout getSelectorPopup(){
+		return selector;
+	}
+	
+	
 	private class RightClickHandler implements ShowContextMenuHandler {
 		@Override
 		public void onShowContextMenu(ShowContextMenuEvent event) {
+			event.cancel();
+			//SC.say("right click");
 			// TODO Auto-generated method stub
-			PopupPanel p = new PopupPanel();
-			p.add(op.getPropertiesSelector());
-			p.show();
-			p.hide();
+			selector.show();
+		}
+	}
+	private class HideSelectorHandler implements ClickHandler{
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			selector.hide();
 		}
 	}
 	
