530db946f715d607e08a38a933e7ba918eaceca7
jveldridge <jveldridge@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-04-28 01:59:52 +0000

    Changes PropertiesSelector so that the ok button is a protected instance variable form element, which each subclass can then set as its final form field.  (Default ClickHandler, which calls validateForm and processInput methods, is still implemented in the superclass).

--- trunk/siliclone/src/edu/brown/cs32/siliclone/operators/PropertiesSelector.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/operators/PropertiesSelector.java
@@ -1,15 +1,16 @@
 package edu.brown.cs32.siliclone.operators;
 
-import com.smartgwt.client.widgets.Button;
-import com.smartgwt.client.widgets.EdgedCanvas;
-import com.smartgwt.client.widgets.events.ClickEvent;
-import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.widgets.form.DynamicForm;
+import com.smartgwt.client.widgets.form.fields.ButtonItem;
+import com.smartgwt.client.widgets.form.fields.events.ClickEvent;
+import com.smartgwt.client.widgets.form.fields.events.ClickHandler;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 public abstract class PropertiesSelector extends VLayout {
 	
 	protected DynamicForm _form;
+	protected ButtonItem _okButton;
 
 	public PropertiesSelector() {
 		_form = new DynamicForm();
@@ -18,8 +19,8 @@ public abstract class PropertiesSelector extends VLayout {
 		this.setEdgeSize(5);
 		this.setEdgeBackgroundColor("#000000");
 		this.setOpacity(20);
-		Button okButton = new Button("OK");
-		okButton.addClickHandler(new ClickHandler() {
+		_okButton = new ButtonItem("OK");
+		_okButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				if (verifyFields()) {
 					processInput();
@@ -27,8 +28,8 @@ public abstract class PropertiesSelector extends VLayout {
 			}
 		});
 		
+		_okButton.setAlign(Alignment.CENTER);
 		this.addMember(_form);
-		this.addMember(okButton);
 	}
 	
 	protected abstract boolean verifyFields();
