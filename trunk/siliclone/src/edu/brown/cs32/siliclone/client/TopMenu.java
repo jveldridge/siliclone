55d59e66fa8f8370afcf9b263cdfb2bf5b37e7d2
jveldridge <jveldridge@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-04-30 06:36:24 +0000

    Progress towards loading Workspaces.  To be continued tomorrow.

--- trunk/siliclone/src/edu/brown/cs32/siliclone/client/TopMenu.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/client/TopMenu.java
@@ -1,16 +1,25 @@
 package edu.brown.cs32.siliclone.client;
 
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.Overflow;
+import com.smartgwt.client.types.SelectionStyle;
 import com.smartgwt.client.util.SC;
 import com.smartgwt.client.util.ValueCallback;
+import com.smartgwt.client.widgets.Button;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.Dialog;
 import com.smartgwt.client.widgets.Img;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.grid.ListGrid;
+import com.smartgwt.client.widgets.grid.ListGridField;
+import com.smartgwt.client.widgets.grid.ListGridRecord;
+import com.smartgwt.client.widgets.grid.events.SelectionChangedHandler;
+import com.smartgwt.client.widgets.grid.events.SelectionEvent;
 import com.smartgwt.client.widgets.toolbar.ToolStrip;
 import com.smartgwt.client.widgets.toolbar.ToolStripButton;
 
@@ -75,17 +84,67 @@ public class TopMenu extends ToolStrip {
 	
 	private class LoadClickHandler implements ClickHandler {
 		
-		private Dialog openDialog;
+		private Dialog _dialog;
+		private ListGrid _sequenceGrid;
+		private DataServiceAsync _service;
+		private String _workspaceToLoad;
 		
 		public LoadClickHandler() {
-			openDialog = new Dialog();
+			_dialog = new Dialog();
+			_dialog.setSize("400px","300px");
+			_dialog.setTitle("Load workspace...");
+			
+			_sequenceGrid = new ListGrid();
+			_sequenceGrid.setSelectionType(SelectionStyle.SINGLE);
+			_sequenceGrid.setShowAllRecords(true);
+			_sequenceGrid.addSelectionChangedHandler(new SelectionChangedHandler() {			
+				public void onSelectionChanged(SelectionEvent event) {
+					_workspaceToLoad = _sequenceGrid.getSelection()[0].getAttribute("name");
+				}
+			});
+			
+			ListGridField name = new ListGridField("name", "Workspace Name");
+			_sequenceGrid.setFields(name);
+			
+			Button okButton = new Button("Load");
+			okButton.addClickHandler(new ClickHandler() {	
+				public void onClick(ClickEvent event) {
+					SC.say(_workspaceToLoad);
+				}
+			});
+			
+			_dialog.addItem(_sequenceGrid);
+			_dialog.addItem(okButton);
+								
+			_service = GWT.create(DataService.class);
 		}
 		
 		public void onClick(ClickEvent event) {
-			openDialog.show();
+			
+			
+			AsyncCallback<List<String>> callback = new AsyncCallback<List<String>>() {
+				public void onFailure(Throwable caught) {
+					SC.say("failure");
+					caught.printStackTrace();
+				}
+
+				public void onSuccess(List<String> result) {
+					for (String w : result) {
+						ListGridRecord rec = new ListGridRecord();
+						rec.setAttribute("name", w);
+						_sequenceGrid.addData(rec);
+					}
+					_dialog.show();
+				}
+				
+			};
+			_service.getAvailableWorkspaces(callback);
 		}
 	}
 	
+	/**
+	 * ClickHandler for the "Save" workspace button.
+	 */
 	private class SaveClickHandler implements ClickHandler {
 		private DataServiceAsync _service;
 		private AsyncCallback<Boolean> _callback;
@@ -112,6 +171,8 @@ public class TopMenu extends ToolStrip {
 						try {
 							_service.saveWorkspace(w, w.getName(), _callback);
 						} catch (Exception e) {
+							//TODO catch an appropriate exception for when a workspace with the
+							//	   passed name already exists, and display an appropriate message to the user
 							e.printStackTrace();
 						}
 					}
@@ -122,6 +183,9 @@ public class TopMenu extends ToolStrip {
 		}
 	}
 	
+	/**
+	 * ClickHandler for the "New" workspace button
+	 */
 	private class NewClickHandler implements ClickHandler {
 		public void onClick(ClickEvent event) {
 			_main.makeNewWorkspace();
