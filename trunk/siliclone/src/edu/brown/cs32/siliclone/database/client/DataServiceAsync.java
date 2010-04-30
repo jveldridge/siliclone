794f1dee4998692e180178268325e67be7aa7441
jveldridge <jveldridge@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-04-30 04:37:53 +0000

    Adds forgotten @RemoteServiceRelativePath() to DataService!

--- trunk/siliclone/src/edu/brown/cs32/siliclone/database/client/DataServiceAsync.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/database/client/DataServiceAsync.java
@@ -11,13 +11,8 @@ import edu.brown.cs32.siliclone.dna.SequenceHook;
 
 public interface DataServiceAsync {
 
-
-	void saveSequence(DNASequence s, String name, AsyncCallback<Boolean> callback);
-
 	void getAvailableSequences(AsyncCallback<List<SequenceHook>> callback);
 
-	
-	
 	void saveWorkspace(Workspace w, String name, AsyncCallback<Boolean> callback);
 
 	void findWorkspace(String name, AsyncCallback<Workspace> callback);
