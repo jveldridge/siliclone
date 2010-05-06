635688e59064971790e906bfe0bf961fca7513a7
noah.langowitz <noah.langowitz@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-05-06 12:18:02 +0000

    renaming serverside PCR2 operators

---
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/server/operators/pcr/PCROperatorComputer.java
@@ -0,0 +1,34 @@
+package edu.brown.cs32.siliclone.server.operators.pcr;
+
+import java.util.Collection;
+import java.util.Map;
+
+import edu.brown.cs32.siliclone.client.dna.SequenceHook;
+import edu.brown.cs32.siliclone.server.TasksDelegation;
+import edu.brown.cs32.siliclone.server.operators.abstractoperator.OperatorComputer;
+import edu.brown.cs32.siliclone.tasks.PCRTask;
+
+public class PCROperatorComputer implements OperatorComputer {
+
+	private int progress;
+	
+	public void cancel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Collection<SequenceHook> computeOutput(
+			Collection<SequenceHook>[] input, Map properties) {
+		PCRTask task = new PCRTask(input, properties);
+		progress = 25;
+		task.compute();
+		progress = 100;
+		return task.getOutput();
+	}
+	
+	public int getProgress() {
+		// TODO Auto-generated method stub
+		return progress;
+	}
+
+}
