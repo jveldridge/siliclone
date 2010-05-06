f835eb45e4b7b86a1a2cc4fd44857e718195b493
jveldridge <jveldridge@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-05-06 14:56:57 +0000

    Makes sequencevisualizer show all sequences in collection, not just the first.

--- trunk/siliclone/src/edu/brown/cs32/siliclone/client/visualizers2/SequenceVisualizer.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/client/visualizers2/SequenceVisualizer.java
@@ -1,12 +1,15 @@
 package edu.brown.cs32.siliclone.client.visualizers2;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Label;
+import com.smartgwt.client.types.TitleOrientation;
 import com.smartgwt.client.util.SC;
-import com.smartgwt.client.widgets.Button;
+import com.smartgwt.client.widgets.form.DynamicForm;
+import com.smartgwt.client.widgets.form.fields.StaticTextItem;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 import edu.brown.cs32.siliclone.client.WorkspaceView;
@@ -17,40 +20,69 @@ import edu.brown.cs32.siliclone.operators.Operator;
 
 public class SequenceVisualizer extends VisualizerCanvas {
 
-	private Label contents;
+	private StaticTextItem contents;
+	private SequenceServiceAsync service;
+	private Map<SequenceHook, String> sequences;
+	
 	public SequenceVisualizer(WorkspaceView workspace, Operator owner) {
 		super(workspace, owner);
 	}
 	
 	public void update() {
 		if(contents == null){
-			VLayout layout = new VLayout();
-			contents = new Label();
-			contents.setText("");
-			layout.addMember(contents);
+			final VLayout layout = new VLayout();
+			final DynamicForm form = new DynamicForm();
+			contents = new StaticTextItem("Sequence(s)");
+			contents.setWidth(150);
+			contents.setWrap(true);
+			contents.setTitleOrientation(TitleOrientation.TOP);
+			form.setWidth100();
+			form.setFields(contents);
+			layout.addChild(form);
 			this.addChild(layout);
 		}
-		Collection<SequenceHook> seqs = owner.getOutputSequence();
-		
-		if(seqs == null || seqs.isEmpty()){
-			contents.setText("Add more input to get results from this operator.");
-		}else {
-			final Button string = new Button("hi");
-			AsyncCallback<String> callback = new AsyncCallback<String>() {
-				public void onFailure(Throwable caught) {
-					SC.say(caught.getMessage());
-					System.out.println("failing in Sqeuencevsiauli");
-				}
-				
-				public void onSuccess(String result) {
-					contents.setText(result);
-				}
-			};
-			System.out.println("about to make RPC in sequencevisualizer update method");
-			((SequenceServiceAsync) GWT.create(SequenceService.class)).
-						getNucleotides(seqs.iterator().next(), callback);
+		System.out.println("update called");
+		System.out.println("contents: " + contents);
+		if (service == null) {
+			service = GWT.create(SequenceService.class);
 		}
-  		this.redraw();
+		
+		final Collection<SequenceHook> seqs = owner.getOutputSequence();
+
+		if(seqs == null || seqs.isEmpty()) {
+			contents.setValue("Add more input to get results from this operator.");
+		}
+		
+		else {
+			sequences = new HashMap<SequenceHook, String>();
+			
+			for (final SequenceHook hook : seqs) {
+				AsyncCallback<String> callback = new AsyncCallback<String>() {
+					public void onFailure(Throwable caught) {
+						SC.say(caught.getMessage());
+					}
+					
+					public void onSuccess(String result) {
+						sequences.put(hook, result);
+						
+						SequenceVisualizer.super.setProgress(100 * (sequences.size() / seqs.size()));
+
+						if (sequences.size() == seqs.size()) {
+							String text = "";
+							for (SequenceHook hook : sequences.keySet()) {
+								text += hook.getSeqName() + ":\n" + sequences.get(hook) + "\n\n";
+							}
+							contents.setValue(text);
+					  		
+							DynamicForm form = new DynamicForm();
+							form.setFields(contents);
+						}
+					}
+				};
+	
+				service.getNucleotides(hook, callback);
+			}
+		}		
 	}
 	
 	
