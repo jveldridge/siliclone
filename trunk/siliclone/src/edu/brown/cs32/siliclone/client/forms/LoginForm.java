b8924ae0532103bf4ae69450f7ad89dafd418e7d
jveldridge <jveldridge@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-05-06 12:36:31 +0000

    Makes logging out destroy exisiting MainView.

--- trunk/siliclone/src/edu/brown/cs32/siliclone/client/forms/LoginForm.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/client/forms/LoginForm.java
@@ -4,7 +4,6 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.util.SC;
-import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.ButtonItem;
 import com.smartgwt.client.widgets.form.fields.FormItem;
@@ -73,7 +72,7 @@ public class LoginForm extends DynamicForm {
 			}
 			
 			public void onSuccess(User result) {
-				_main.showMainView();
+				_main.login();
 			}
 		};
 	
@@ -82,7 +81,6 @@ public class LoginForm extends DynamicForm {
 		} catch (DataServiceException e) {
 			SC.say(e.getMessage());
 		}
-		//_main.showMainView();
 	}
 
 }
