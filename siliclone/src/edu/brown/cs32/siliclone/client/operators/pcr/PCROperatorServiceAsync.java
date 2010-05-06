b3560758bb45360b42385697db30599cbeaab119
noah.langowitz <noah.langowitz@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-05-06 12:17:23 +0000

    renaming PCR2 operator and associates to PCR

---
+++ siliclone/src/edu/brown/cs32/siliclone/client/operators/pcr/PCROperatorServiceAsync.java
@@ -0,0 +1,8 @@
+package edu.brown.cs32.siliclone.client.operators.pcr;
+
+import edu.brown.cs32.siliclone.client.operators.abstractremoteoperator.AbstractRemoteOperatorServiceAsync;
+
+public interface PCROperatorServiceAsync extends
+		AbstractRemoteOperatorServiceAsync {
+
+}
---
+++ siliclone/src/edu/brown/cs32/siliclone/client/operators/pcr/PCROperatorTemplate.java
@@ -0,0 +1,25 @@
+package edu.brown.cs32.siliclone.client.operators.pcr;
+
+import com.google.gwt.user.client.ui.Widget;
+
+import edu.brown.cs32.siliclone.client.operators.abstractremoteoperator.AbstractRemoteOperatorTemplate;
+import edu.brown.cs32.siliclone.operators.Operator;
+
+public class PCROperatorTemplate extends AbstractRemoteOperatorTemplate {
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "PCR2";
+	}
+
+	public Widget getWidget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Operator makeOperator() {
+		// TODO Auto-generated method stub
+		return new PCROperator();
+	}
+
+}
