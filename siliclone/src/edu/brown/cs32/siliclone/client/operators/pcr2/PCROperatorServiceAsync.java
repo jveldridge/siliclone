1848e8f62035000a51eac248d8fffd22cd8d8222
noah.langowitz <noah.langowitz@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-05-06 12:18:45 +0000

    deleting pcr2 package

--- siliclone/src/edu/brown/cs32/siliclone/client/operators/pcr2/PCROperatorServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package edu.brown.cs32.siliclone.client.operators.pcr2;
-
-import edu.brown.cs32.siliclone.client.operators.abstractremoteoperator.AbstractRemoteOperatorServiceAsync;
-
-public interface PCROperatorServiceAsync extends
-		AbstractRemoteOperatorServiceAsync {
-
-}
