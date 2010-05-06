236510208883e9916725c20e80439538d70b3a56
noah.langowitz <noah.langowitz@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-05-06 12:19:26 +0000

    makes PCR operator check both orientations of primers

--- trunk/siliclone/src/edu/brown/cs32/siliclone/plugins/Plugins.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/plugins/Plugins.java
@@ -1,7 +1,7 @@
 package edu.brown.cs32.siliclone.plugins;
 
 import edu.brown.cs32.siliclone.client.operators.restrictiondigest.DigestTemplate;
-import edu.brown.cs32.siliclone.client.operators.pcr2.PCR2OperatorTemplate;
+import edu.brown.cs32.siliclone.client.operators.pcr.PCROperatorTemplate;
 import edu.brown.cs32.siliclone.client.operators.slowoperator.SlowOperatorTemplate;
 import edu.brown.cs32.siliclone.client.visualizers2.DataVisualizerTemplate;
 import edu.brown.cs32.siliclone.client.visualizers2.SequenceVisualizerTemplate;
@@ -10,17 +10,15 @@ import edu.brown.cs32.siliclone.client.visualizers2.translation.TranslationVisua
 import edu.brown.cs32.siliclone.operators.OperatorAdder;
 import edu.brown.cs32.siliclone.operators.anothertestop.AnotherTestOpFactory;
 import edu.brown.cs32.siliclone.operators.client.dnaInput.DNAInputTemplate;
-import edu.brown.cs32.siliclone.operators.pcr.PCRTemplate;
 
 public class Plugins {
 
 	public static void defineOperators(OperatorAdder operatorAdder){
 		operatorAdder.addOperator(new AnotherTestOpFactory());
-		operatorAdder.addOperator(new PCRTemplate());
 		operatorAdder.addOperator(new DNAInputTemplate());
 		operatorAdder.addOperator(new DigestTemplate());
 		operatorAdder.addOperator(new SlowOperatorTemplate());
-		operatorAdder.addOperator(new PCR2OperatorTemplate());
+		operatorAdder.addOperator(new PCROperatorTemplate());
 	}
 	
 	public static void defineVisualizers(VisualizerAdder visualizerAdder){
