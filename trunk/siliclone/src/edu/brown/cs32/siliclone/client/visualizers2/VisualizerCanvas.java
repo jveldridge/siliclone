71142b1995c7f4f2561082fb7b9f6a4e0d972c8e
douglas_hall@brown.edu <douglas_hall@brown.edu@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-05-06 14:18:40 +0000

    visualizer canvases now have progress bars with public updating methods

--- trunk/siliclone/src/edu/brown/cs32/siliclone/client/visualizers2/VisualizerCanvas.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/client/visualizers2/VisualizerCanvas.java
@@ -1,6 +1,7 @@
 package edu.brown.cs32.siliclone.client.visualizers2;
 
 import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.Progressbar;
 
 import edu.brown.cs32.siliclone.client.WorkspaceView;
 import edu.brown.cs32.siliclone.operators.Operator;
@@ -8,12 +9,40 @@ import edu.brown.cs32.siliclone.operators.Operator;
 public abstract class VisualizerCanvas extends Canvas {
 	protected WorkspaceView workspace;
 	protected Operator owner;
+	private Progressbar progress;
 	
 	
 	public VisualizerCanvas(WorkspaceView workspace, Operator owner){
 		this.workspace = workspace;
 		this.owner = owner;
 		update();
+		
+
+		progress = new Progressbar();
+        progress.setBreadth(15);
+        progress.setLength(getWidth() - 20);
+        addChild(progress);
+        progress.setLeft(5);
+        progress.setTop(getHeight() - progress.getHeight());
+        progress.hide();
+	}
+	
+	
+
+	public void showProgressBar(){
+		progress.show();
+	}
+	
+	public void hideProgressBar(){
+		progress.hide();
+	}
+	public void setProgress(int percent){
+		if(percent >= 100){
+			hideProgressBar();
+		}else{
+			showProgressBar();
+			progress.setPercentDone(percent);
+		}
 	}
 	
 	public abstract void update();
