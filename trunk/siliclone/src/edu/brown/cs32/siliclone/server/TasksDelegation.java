b83f25c80b629cb8fe398bfa57998f5ca89fab27
tristan.de.rond <tristan.de.rond@c8bbeddc-0de9-caf8-e227-5dc5d942b2bb>
2010-05-11 00:18:49 +0000

    using Tasks framework now

--- trunk/siliclone/src/edu/brown/cs32/siliclone/server/TasksDelegation.java
+++ trunk/siliclone/src/edu/brown/cs32/siliclone/server/TasksDelegation.java
@@ -1,8 +1,12 @@
 package edu.brown.cs32.siliclone.server;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 import edu.brown.cs32.siliclone.database.server.IndexNucleotideSequenceTask;
 import edu.brown.cs32.siliclone.tasks.Task;
 import edu.brown.cs32.siliclone.tasks.client.TaskClient;
+import edu.brown.cs32.siliclone.tasks.client.TaskTimedOutException;
 
 public class TasksDelegation {
 	
@@ -12,8 +16,26 @@ public class TasksDelegation {
 	 * @return
 	 */
 	public static Task delegate(Task t){
-		t.compute();
+		
+		
+		try {
+			TaskClient tc = new TaskClient("caladan.cs.brown.edu", 2010);
+			t=tc.computeTask(t);
+			tc.close();
+		} catch (UnknownHostException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		} catch (TaskTimedOutException e) {
+			// TODO Auto-generated catch block
+			System.err.println("TASK TIMED OUT!");
+		}
 		return t;
+		
+		
+		
 	}
 
 }
